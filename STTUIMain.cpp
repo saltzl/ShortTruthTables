1b76a9d6de033ca3cef16abf91ce3be21b6b8d9d
Lee Saltzman <mccall.saltzman@gmail.com>
2015-05-15 11:42:44 -0400

    Finished program

--- STTUIMain.cpp
+++ STTUIMain.cpp
@@ -11,12 +11,79 @@ Glib::RefPtr<Gtk::Application> app;
 Gtk::Window* mainWindow = 0;
 Gtk::Button* premiseButton;
 Gtk::Button* conclusionButton;
+ShortTruthTables::STTModel* model;
+
+void solve(){
+  Gtk::CheckButton* checkButton;
+  refBuilder->get_widget("checkbutton1", checkButton);
+  char c;
+  if(checkButton->get_active()){
+    c = 'v';
+  }else{
+    c = 'i';
+  }
 
 
+  if(c == 'v'){
+    for(int i = 0; i < model->getNumPremises(); i++){
+      for(int j = 0; j < model->getPremise(i)->getNumExpressions(); j++){
+        bool flag = (model->getPremise(i)->getNthExpression(j)->isValid());
+        if(!flag && !model->getPremise(i)->getNthExpression(j)->isUnassigned()){
+            Gtk::MessageDialog *result = new Gtk::MessageDialog("Argument is valid, cannot make all premises true with conclusion false");
+            int response = result->run();
+            result->hide();
+            return;
+        }
+      }
+    }
+
+    for(int i = 0; i < model->getConclusion()->getNumExpressions(); i++){
+      bool flag = (model->getConclusion()->getNthExpression(i)->isValid());
+        if(!flag && !model->getConclusion()->getNthExpression(i)->isUnassigned()){
+            Gtk::MessageDialog *result = new Gtk::MessageDialog("Argument is valid, cannot make all premises true with conclusion false");
+            int response = result->run();
+            result->hide();
+          return;
+        }
+    }
+     Gtk::MessageDialog *result = new Gtk::MessageDialog("Argument is not currently valid");
+     int response = result->run();
+     result->hide();
+    return;
+  }else if( c == 'i'){
+    for(int i = 0; i < model->getNumPremises(); i++){
+      for(int j = 0; j < model->getPremise(i)->getNumExpressions(); j++){
+        bool flag = (!model->getPremise(i)->getNthExpression(j)->isUnassigned() && model->getPremise(i)->getNthExpression(j)->isValid());
+        if(!flag){
+            Gtk::MessageDialog *result = new Gtk::MessageDialog("argument is not currently invalid");
+            int response = result->run();
+            result->hide();
+          return;
+        }
+      }
+    }
+
+    for(int i = 0; i < model->getConclusion()->getNumExpressions(); i++){
+      bool flag = (!model->getConclusion()->getNthExpression(i)->isUnassigned() && model->getConclusion()->getNthExpression(i)->isValid());
+        if(!flag){
+            Gtk::MessageDialog *result = new Gtk::MessageDialog("argument is not currently invalid");
+            int response = result->run();
+            result->hide();
+          return;
+        }
+    }
+     Gtk::MessageDialog *result = new Gtk::MessageDialog("Argument is indeed invalid, all premises true with conclusion false");
+      int response = result->run();
+     result->hide();
+    return;
+  }else{
+    return;
+  }
+}
 
 int main (int argc, char **argv)
 {
-  ShortTruthTables::STTModel* model = new ShortTruthTables::STTModel();
+  model = new ShortTruthTables::STTModel();
   app = Gtk::Application::create(argc, argv, "com.rpi.saltzl.stt");
 
   //Load the GtkBuilder file and instantiate its widgets:
@@ -54,6 +121,9 @@ int main (int argc, char **argv)
     refBuilder->get_widget("concButton", conclusionButton);
     conclusionButton->signal_clicked().connect( sigc::mem_fun(concView, &ConcDialog::show) );
 
+    Gtk::Button* but5;
+    refBuilder->get_widget("button5", but5);
+    but5->signal_clicked().connect( sigc::ptr_fun(&solve) );
 
     app->run(*mainWindow);
   }
